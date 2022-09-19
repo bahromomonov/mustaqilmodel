a4cde466718a35e91c0307d2fbddbbd3fe5ac4ad
bahromomonov97 <94924148+bahromomonov@users.noreply.github.com>
2022-09-19 22:41:26 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from fastai.vision.all import *
 import streamlit as st
-import plotly.express as px
 import pathlib
-temp=pathlib.PosixPath
-pathlib.PosixPath=pathlib.WindowsPath
+import plotly.express as px
+import platform 
+plt=platform.system()
+if plt=='Linux':pathlib.WindowsPath=pathlib.PosixPath
 st.title('Buyumlarni klassifikatsiyalovchi model')
 file=st.file_uploader("Rasm yuklash:,type=['jpeg','jpg','svg','gif']")
 if file:
